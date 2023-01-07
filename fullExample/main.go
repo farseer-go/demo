75418339678df9f405578a7ed6c03f7845ba5509
steden <1470804@qq.com>
2023-01-07 21:58:41 +0800

    调整configure.ParseConfig -> configure.ParseString函数名称

--- fullExample/main.go
+++ fullExample/main.go
@@ -10,7 +10,9 @@ func main() {
 	fs.Initialize[StartupModule]("demo")
 
 	webapi.Area("/api/1.0/", func() {
-		webapi.RegisterPOST("/product/ToEntity", productApp.ToEntity)
+		webapi.RegisterGET("/product/info", productApp.ToEntity)
+		webapi.RegisterGET("/product/info/{productId}", productApp.ToEntity)
+		webapi.RegisterGET("/product/list-{pageSize}-{pageIndex}", productApp.ToList, "pageIndex", "pageSize", "repository")
 	})
 	webapi.UseApiResponse()
 	webapi.Run()
