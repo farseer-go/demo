24e19519b622542f7e1059f8a3ac9b86ae897a9a
steden <1470804@qq.com>
2023-09-27 23:32:15 +0800

    调整：自动创建表开关改到上下文的tag中 新增：支持表引擎设置

--- shopping/infrastructure/repository/context/mysqlContext.go
+++ shopping/infrastructure/repository/context/mysqlContext.go
@@ -19,14 +19,14 @@ type mysqlContext struct {
 	// 获取原生ORM框架（不使用TableSet或DomainSet）
 	data.IInternalContext
 	// 定义数据库表 订单 映射TableSet
-	Order data.DomainSet[model.OrderPO, order.DomainObject] `data:"name=farseer_go_order"`
+	Order data.DomainSet[model.OrderPO, order.DomainObject] `data:"name=farseer_go_order;migrate"`
 	// 定义数据库表 商品分类 映射TableSet
-	ProCate data.DomainSet[model.ProCatePO, procate.DomainObject] `data:"name=farseer_go_procate"`
+	ProCate data.DomainSet[model.ProCatePO, procate.DomainObject] `data:"name=farseer_go_procate;migrate"`
 	// 定义数据库表 产品 映射TableSet
-	Product data.DomainSet[model.ProductPO, product.DomainObject] `data:"name=farseer_go_product"`
+	Product data.DomainSet[model.ProductPO, product.DomainObject] `data:"name=farseer_go_product;migrate"`
 }
 
 // InitMysqlContext 初始化上下文
 func InitMysqlContext() {
-	MysqlContext = data.NewContext[mysqlContext]("default", true)
+	MysqlContext = data.NewContext[mysqlContext]("default")
 }
